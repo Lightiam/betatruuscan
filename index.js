548faa12684a9a3e738a66971bd99d80658b2712
Satvik Kushwaha <satvik213161@gmail.com>
2022-01-27 22:40:02 +0530

    d

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const port = process.env.PORT || 3000
 
-app.get('/',(req,res)=>{
-    res.send('working');
-    console.log("working");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
 });
 
-app.listen(process.env.PORT||3000,()=>{console.log("hua chalu hua");});
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
