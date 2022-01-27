141682fcd5e783367190a8abe2b69a7e4f2d3802
Satvik Kushwaha <satvik213161@gmail.com>
2022-01-27 22:35:56 +0530

    weoo

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
+    res.send('working');
     console.log("working");
 });
 
-app.listen(porcess.env.PORT||3000,()=>{console.log("hua chalu hua");});
+app.listen(process.env.PORT||3000,()=>{console.log("hua chalu hua");});
